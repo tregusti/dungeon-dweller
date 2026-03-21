8589ec0e9846de07b3f0fba1eb130caf8eb74346
Glenn Jorde <glenn@tregusti.com>
2026-03-21 12:04:03 +0100

    remove unneeded main function

--- src/index.ts
+++ src/index.ts
@@ -30,155 +30,151 @@ const monsters = new MonsterCollection()
 const dungeon = new Dungeon(dungeonSize, hero, monsters)
 const game = new Game(dungeon, status, hero, monsters)
 
-function main() {
-  const terminal = new Terminal(game.width, game.height)
-  Debug.initialize({ terminal, game })
+const terminal = new Terminal(game.width, game.height)
+Debug.initialize({ terminal, game })
 
-  const compositor = new BufferCompositor({
-    width: game.width,
-    height: game.height,
-  })
-  const dungeonBuffer = compositor.add({
-    buffer: new Buffer({
-      width: game.dungeon.width,
-      height: game.dungeon.height,
-    }),
-    x: 0,
-    y: 0,
-    z: 0,
-  })
-  compositor.add({
-    buffer: status.buffer,
-    x: 0,
-    y: game.dungeon.height + 1,
-    z: 1,
-  })
+const compositor = new BufferCompositor({
+  width: game.width,
+  height: game.height,
+})
+const dungeonBuffer = compositor.add({
+  buffer: new Buffer({
+    width: game.dungeon.width,
+    height: game.dungeon.height,
+  }),
+  x: 0,
+  y: 0,
+  z: 0,
+})
+compositor.add({
+  buffer: status.buffer,
+  x: 0,
+  y: game.dungeon.height + 1,
+  z: 1,
+})
 
-  const renderer = new Renderer(dungeonBuffer, compositor, terminal)
-  renderer.attach()
+const renderer = new Renderer(dungeonBuffer, compositor, terminal)
+renderer.attach()
 
-  // IDEA:
-  // status manages its own buffer and implements the CompositorRegistrant interface
-  // which is a method that compositor invokes and passes the buffer to it when it's registered.
-  // const status = new Status({ game })
-  // compositor.register(status)
+// IDEA:
+// status manages its own buffer and implements the CompositorRegistrant interface
+// which is a method that compositor invokes and passes the buffer to it when it's registered.
+// const status = new Status({ game })
+// compositor.register(status)
 
-  /** @deprecated */
-  function forceRedraw() {
-    terminal.clear()
-    flushBuffer(terminal, compositor)
-  }
+/** @deprecated */
+function forceRedraw() {
+  terminal.clear()
+  flushBuffer(terminal, compositor)
+}
 
-  // initial render
-  dungeonBuffer.clear()
-  dungeonBuffer.set(game.hero.x, game.hero.y, game.hero.char)
-  status.update(game)
+// initial render
+dungeonBuffer.clear()
+dungeonBuffer.set(game.hero.x, game.hero.y, game.hero.char)
+status.update(game)
 
-  const moveHeroCollisionService = new MoveHeroCollisionService(
-    game.dungeon,
-    game.monsters,
-  )
-  const moveHeroCommandHandler = new MoveHeroCommandHandler(
-    game.hero,
-    moveHeroCollisionService,
-    Bus.event,
-  )
-  Bus.command.register(CommandType.MoveHero, (payload) =>
-    moveHeroCommandHandler.handle(payload),
-  )
+const moveHeroCollisionService = new MoveHeroCollisionService(
+  game.dungeon,
+  game.monsters,
+)
+const moveHeroCommandHandler = new MoveHeroCommandHandler(
+  game.hero,
+  moveHeroCollisionService,
+  Bus.event,
+)
+Bus.command.register(CommandType.MoveHero, (payload) =>
+  moveHeroCommandHandler.handle(payload),
+)
 
-  const createMonsterCommandHandler = new CreateMonsterCommandHandler(
-    game.dungeon,
-    game.monsters,
-    Random,
-  )
-  Bus.command.register(CommandType.CreateMonster, () =>
-    createMonsterCommandHandler.handle(),
-  )
+const createMonsterCommandHandler = new CreateMonsterCommandHandler(
+  game.dungeon,
+  game.monsters,
+  Random,
+)
+Bus.command.register(CommandType.CreateMonster, () =>
+  createMonsterCommandHandler.handle(),
+)
 
-  Bus.event.subscribe(EventType.HeroMoved, () => {
-    game.advanceTurn()
-  })
+Bus.event.subscribe(EventType.HeroMoved, () => {
+  game.advanceTurn()
+})
 
-  let gameEnabled = false
+let gameEnabled = false
 
-  terminal.on('invalid', () => {
-    gameEnabled = false
-  })
+terminal.on('invalid', () => {
+  gameEnabled = false
+})
 
-  terminal.on('valid', () => {
-    forceRedraw()
-    gameEnabled = true
-  })
-
-  terminal.on('input', onInput)
-
+terminal.on('valid', () => {
   forceRedraw()
   gameEnabled = true
+})
 
-  async function onInput(chunk: string) {
-    if (chunk === '\u0003' || chunk === 'q') {
-      // ctrl-c
-      terminal.exit()
-    }
+terminal.on('input', onInput)
 
-    if (!gameEnabled) {
-      return
-    }
+forceRedraw()
+gameEnabled = true
 
-    switch (chunk) {
-      // movement keys
-      case 'h':
-        await handleMovement(-1, 0)
-        break
-      case 'l':
-        await handleMovement(1, 0)
-        break
-      case 'k':
-        await handleMovement(0, -1)
-        break
-      case 'j':
-        await handleMovement(0, 1)
-        break
-      // actions
-      case 'm':
-        await handleCreateMonster()
-        break
-    }
-
-    status.update(game)
+async function onInput(chunk: string) {
+  if (chunk === '\u0003' || chunk === 'q') {
+    // ctrl-c
+    terminal.exit()
   }
 
-  async function handleCreateMonster() {
-    const result = await Bus.command.execute(CommandType.CreateMonster)
-
-    if (!result.success) {
-      Debug.write(`No free dungeon tile to spawn monster at turn ${game.turns}`)
-      return
-    }
+  if (!gameEnabled) {
+    return
   }
 
-  async function handleMovement(dx: number, dy: number) {
-    const result = await Bus.command.execute(CommandType.MoveHero, {
-      dx,
-      dy,
-    })
+  switch (chunk) {
+    // movement keys
+    case 'h':
+      await handleMovement(-1, 0)
+      break
+    case 'l':
+      await handleMovement(1, 0)
+      break
+    case 'k':
+      await handleMovement(0, -1)
+      break
+    case 'j':
+      await handleMovement(0, 1)
+      break
+    // actions
+    case 'm':
+      await handleCreateMonster()
+      break
+  }
 
-    if (result.success) {
-      do {
-        game.processMonsterTurn()
-        game.hero.giveEnergy()
-      } while (game.hero.energy < game.hero.speed)
-    } else {
-      if (result.reason === 'wall') {
-        Debug.write(`Hero bumps into a wall at turn ${game.turns}`)
-      } else if (result.reason === 'monster') {
-        Debug.write(
-          `Hero bumps into a ${result.monster.char} at turn ${game.turns}`,
-        )
-      }
-    }
+  status.update(game)
+}
+
+async function handleCreateMonster() {
+  const result = await Bus.command.execute(CommandType.CreateMonster)
+
+  if (!result.success) {
+    Debug.write(`No free dungeon tile to spawn monster at turn ${game.turns}`)
+    return
   }
 }
 
-main()
+async function handleMovement(dx: number, dy: number) {
+  const result = await Bus.command.execute(CommandType.MoveHero, {
+    dx,
+    dy,
+  })
+
+  if (result.success) {
+    do {
+      game.processMonsterTurn()
+      game.hero.giveEnergy()
+    } while (game.hero.energy < game.hero.speed)
+  } else {
+    if (result.reason === 'wall') {
+      Debug.write(`Hero bumps into a wall at turn ${game.turns}`)
+    } else if (result.reason === 'monster') {
+      Debug.write(
+        `Hero bumps into a ${result.monster.char} at turn ${game.turns}`,
+      )
+    }
+  }
+}
